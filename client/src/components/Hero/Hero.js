44cba5a1600d9ae7331a54952b231f60c6ac703d
Atom-v-d-M <79427852+Atom-v-d-M@users.noreply.github.com>
2021-06-11 10:47:41 -0700

    Added hero image and updated site copy

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -1,6 +1,7 @@
 import './Hero.scss';
 import appleDownload from '../../assets/images/appleDownload.svg'
 import googleDownload from '../../assets/images/googleDownload.svg'
+import hero from '../../assets/images/hero.png'
 
 function Hero () {
     return (
@@ -9,7 +10,7 @@ function Hero () {
                 <article className="primary-hero__body">
                     <div className="primary-hero__copy-container">
                         <h1 className="primary-hero__title">Rescue food, eat for less.</h1>
-                        <p className="primary-hero__subtitle">Rescue food from waste while enjoying a great meal for less.</p>
+                        <p className="primary-hero__subtitle">Rescue food from waste while enjoying a great healthy meal for less.</p>
                         <div className="primary-hero__icon-container">
                             <img src={appleDownload} alt="apple download link" className="primary-hero__image primary-hero__image--apple"/>
                             <img src={googleDownload} alt="google download link" className="primary-hero__image"/>
@@ -17,7 +18,7 @@ function Hero () {
                     </div>
                     <aside className="primary-hero__aside">
                         <figure className="primary-hero__figure">
-                            <img src="#" alt="some alt text" className="primary-hero__hero-image"/>
+                            <img src={hero} alt="view of app on phone" className="primary-hero__hero-image"/>
                         </figure>
                     </aside>
                 </article>
--- client/src/components/SiteBody/SiteBody.js
+++ client/src/components/SiteBody/SiteBody.js
@@ -1,58 +1,79 @@
 import './SiteBody.scss';
+import { Component } from "react";
 import eatingTogether from '../../assets/images/eatingTogether.svg'
 import womanWithPhone from '../../assets/images/womanWithPhone.svg'
 import contract from '../../assets/images/contract.svg'
 import toronto from '../../assets/images/toronto.svg'
 
-function SiteBody () {
-    return (
-        <main className="site-body">
-            <article className="primary-body-section" id="About Us">
-                <div className="primary-body-section__container">
-                    <div className="primary-body-section__body">
-                        <h2 className="primary-body-section__title">What We Do</h2>
-                        <p className="primary-body-section__description primary-body-section__description--about">We save food from being wasted at local Toronto restaurants. Restaurants are a major contributer to food waste. To address this issue, we ensure fresh unused meals are offerred at reduced prices at the end of each day.</p>
-                        <div className="primary-body-section__icon-container">
-                            <img src={toronto} alt="Map marker with text Toronto" className="primary-body-section__icon"/>
+class SiteBody extends Component {
+
+    state = {
+        pageLayout: "default"
+    }
+
+    handleClick = () => {
+        this.setState({
+            pageLayout: "contactForm"
+        })
+    }
+
+    render () {
+        // let pageState = {
+        //     return
+        // }
+
+        if (this.state.pageLayout === "contactForm") {
+
+        }
+        return (
+            <main className="site-body">
+                <article className="primary-body-section" id="About Us">
+                    <div className="primary-body-section__container">
+                        <div className="primary-body-section__body">
+                            <h2 className="primary-body-section__title">What We Do</h2>
+                            <p className="primary-body-section__description primary-body-section__description--about">Mango works to tackle the nation-wide food waste crisis by collaborating with local Toronto restaurants to offer fresh, healthy meals at reduced prices. At the end of the day, instead of throwing out leftover food products, restaurants will offer the remains on the Mango app and connect with hungry Torontonians looking for discounted food.</p>
+                            <div className="primary-body-section__icon-container">
+                                <img src={toronto} alt="Map marker with text Toronto" className="primary-body-section__icon"/>
+                            </div>
                         </div>
+                        <aside className="primary-body-section__aside">
+                            <figure className="primary-body-section__figure">
+                                <img src={eatingTogether} alt="Two people sitting at a table eating food" className="primary-body-section__image primary-body-section__image--about"/>
+                            </figure>
+                        </aside>
                     </div>
-                    <aside className="primary-body-section__aside">
-                        <figure className="primary-body-section__figure">
-                            <img src={eatingTogether} alt="Two people sitting at a table eating food" className="primary-body-section__image primary-body-section__image--about"/>
-                        </figure>
-                    </aside>
-                </div>
-            </article>
-            <article className="primary-body-section">
-                <div className="primary-body-section__container primary-body-section__container--reverse">
-                    <div className="primary-body-section__body primary-body-section__body--theApp">
-                        <h2 className="primary-body-section__title">Try The App</h2>
-                        <p className="primary-body-section__description primary-body-section__description--theApp">Hop on our app to find restaurants near you offering great meals for reduced prices. Instead of going to waste these meals can be yours!</p>
-                    </div>
-                    <aside className="primary-body-section__aside primary-body-section__aside--reverse">
-                        <figure className="primary-body-section__figure">
-                            <img src={womanWithPhone} alt="Two people sitting at a table eating food" className="primary-body-section__image primary-body-section__image--theApp"/>
-                        </figure>
-                    </aside>
-                </div>
-            </article>
-            <article className="primary-body-section primary-body-section--no-margin" id="Contact">
-                <div className="primary-body-section__container">
-                    <div className="primary-body-section__body">
-                        <h2 className="primary-body-section__title">Become A Partner</h2>
-                        <p className="primary-body-section__description primary-body-section__description--contact">Become a partner to reach new customers, save money and reduce waste.</p>
-                        <div className="primary-body-section__button-container">
-                            <a href="#" className="primary-body-section__button">Contact Us</a>
+                </article>
+                <article className="primary-body-section">
+                    <div className="primary-body-section__container primary-body-section__container--reverse">
+                        <div className="primary-body-section__body primary-body-section__body--theApp">
+                            <h2 className="primary-body-section__title">Try The App</h2>
+                            <p className="primary-body-section__description primary-body-section__description--theApp">Discover restaurants near you offering fresh meals at affordable prices, while contributing to a more sustainable world. View the health rating on each meal to give your body the fuel is deserves.</p>
                         </div>
+                        <aside className="primary-body-section__aside primary-body-section__aside--reverse">
+                            <figure className="primary-body-section__figure">
+                                <img src={womanWithPhone} alt="Two people sitting at a table eating food" className="primary-body-section__image primary-body-section__image--theApp"/>
+                            </figure>
+                        </aside>
                     </div>
-                    <aside className="primary-body-section__aside">
-                        <figure className="primary-body-section__figure">
-                            <img src={contract} alt="Two people sitting at a table eating food" className="primary-body-section__image primary-body-section__image--contact"/>
-                        </figure>
-                    </aside>
-                </div>
-            </article>
-        </main>
-    );
+                </article>
+                <article className="primary-body-section primary-body-section--no-margin" id="Contact">
+                    <div className="primary-body-section__container">
+                        <div className="primary-body-section__body">
+                            <h2 className="primary-body-section__title">Become A Partner</h2>
+                            <p className="primary-body-section__description primary-body-section__description--contact">Become a partner to access a wide network of customers from all over the city looking for new, affordable and delicious food. Save money while playing your part in reducing food wastage and shortage while helping the greater good.</p>
+                            <div className="primary-body-section__button-container">
+                                <a href="#" className="primary-body-section__button">Contact Us</a>
+                            </div>
+                        </div>
+                        <aside className="primary-body-section__aside">
+                            <figure className="primary-body-section__figure">
+                                <img src={contract} alt="Two people sitting at a table eating food" className="primary-body-section__image primary-body-section__image--contact"/>
+                            </figure>
+                        </aside>
+                    </div>
+                </article>
+            </main>
+        );
+    }
 };
 export default SiteBody;
